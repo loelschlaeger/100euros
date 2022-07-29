5c02a22d9ca267f3f7dc4b1f1a9350c40e2ad474
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2022-07-29 20:23:26 +0200

    Initial data commit.

---
+++ code.R
@@ -0,0 +1,4 @@
+stocks <- read.csv("stocks.csv", header = TRUE, sep = ";", dec = ",")
+shares <- read.csv("shares.csv", header = TRUE, sep = ";", dec = ",", check.names = FALSE)
+dividends <- read.csv("dividends.csv", header = TRUE, sep = ";", dec = ",", check.names = FALSE)
+
