dcc3640076c13dc269a8f6cfe95eaae29a1509e5
lennart.oelschlaeger <oelschlaeger.lennart@gmail.com>
2022-08-11 14:08:19 +0200

    Removed file.

--- code.R
+++
@@ -1,9 +0,0 @@
-library("dplyr")
-
-overview <- read.csv("data/overview.csv", header = TRUE, sep = ";", dec = ",")
-
-overview <- overview %>% as_tibble() 
-
-overview %>% select(name, wkn, country, sector)
-
-overview %>% select(name, dy, cydi, month)
